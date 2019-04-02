080b90a5ddbc1409f63b5e666de9fff3e6362526
gowachin <gowachin974@gmail.com>
2019-04-02 12:38:41 +0200

    affinage

--- get_subsection_Erythrodrosum.r
+++ get_subsection_Erythrodrosum.r
@@ -4,24 +4,24 @@ mat
 row.names(mat)
 
 int = c('AP1','AMB','AML','AOL','CS1','CP1','CP4','DGB','DRL','DMB','HC1','HGL','HS2','HP1','HPB','PT1','PV1','GA2','GA4','VR3','VR1','VL2','VB1')
-mat2=mat[which(row.names(mat)%in%int),] # with one outgroup: AP1 (P. lutea)
-mat2
+Erythro=mat[which(row.names(mat)%in%int),] # with one outgroup: AP1 (P. lutea)
+Erythro
 
 # subset to keep SNPs only
-as.character(mat2[,1])
+as.character(Erythro[,1])
 
-N_bases=rep(NA,dim(mat2)[2])
+N_bases=rep(NA,dim(Erythro)[2])
 for (i in 1:length(N_bases)){
-  temp=c(unique(as.character(mat2[,i])))
+  temp=c(unique(as.character(Erythro[,i])))
   N_bases[i]=sum(temp!='?')
   #print(i)
 }
 
 table(N_bases)
-as.character(mat2[,which(N_bases==7)])
-mat3=mat2[,which(N_bases>1)]
-mat3
-write.dna(mat3,file='data/freebayes_-F0.3-n10-m30_-q20_mincov20_subsection_Erythrodrosum_23samples_SNPs_only_FASTA.fasta',format='fasta')
+as.character(Erythro[,which(N_bases==7)])
+SNP=Erythro[,which(N_bases>1)]
+SNP
+write.dna(SNP,file='data/freebayes_-F0.3-n10-m30_-q20_mincov20_subsection_Erythrodrosum_23samples_SNPs_only_FASTA.fasta',format='fasta')
 
 
 # sous-groupe 'pedemontana s.l.'
@@ -50,13 +50,23 @@ Occurences_Alps <- read_csv("data_carto/Occurences_Alps.csv")
 #View(Occurences_Alps)
 Occurences_Alps = Occurences_Alps[,-7]
 
+Repart = list(Repart.pedemontana = Occurences_Alps[Occurences_Alps$Taxon == "Primula pedemontana",],
+Repart.villosa = Occurences_Alps[Occurences_Alps$Taxon == "Primula villosa",],
+Repart.hirsuta = Occurences_Alps[Occurences_Alps$Taxon == "Primula hirsuta",],
+Repart.daonensis = Occurences_Alps[Occurences_Alps$Taxon == "Primula daonensis",])
+
 Sub_appendix = Appendix_table[Appendix_table$Recode %in% int,]
-Alpine.ext = extent (-10,25,40,50)
+Alpine.ext = extent (-2,18,42,48)
 plot.raster("data_carto/GRASSLANDS_10min.tif",Alpine.ext,line = T)
-#plot.obj("data_carto/elevation1x1_new.tif")
-par(new = T)
+tail = c(0.1,0.1,0.1,0.1) ; color = c("red","green","grey","purple")
+for (i in 1:length(Repart)) {points(x = Repart[[i]]$lon , y = Repart[[i]]$lat, cex = tail[i], col=color[i]) }
 text(x = Sub_appendix$Latitude, y = Sub_appendix$Longitude, Sub_appendix$Recode, cex = 0.7) #text(x = 22, y = 30, "taille = 22 : seuil de 5%", col = "red", cex = 1.5)
 #points(x = Sub_appendix$Latitude, y = Sub_appendix$Longitude)
-points(x = Occurences_Alps$lon , y = Occurences_Alps$lat, cex = 0.5) # attention problemes de noms entre lon et lat, c'est l'inverse.
 
-
+Pedemonta.ext = extent (5,11,44,46)
+Pede_appendix = Appendix_table[Appendix_table$Recode %in% pedemontana,]
+plot.raster("data_carto/GRASSLANDS_10min.tif",Pedemonta.ext,line = T)
+tail = c(0.1,0.1,0.1,0.1) ; color = c("red","black","grey","purple")
+for (i in 1:length(Repart)) {points(x = Repart[[i]]$lon , y = Repart[[i]]$lat, cex = tail[i], col=color[i]) }
+text(x = Pede_appendix$Latitude, y = Pede_appendix$Longitude, Pede_appendix$Recode, cex = 0.7) #text(x = 22, y = 30, "taille = 22 : seuil de 5%", col = "red", cex = 1.5)
+#points(x = Sub_appendix$Latitude, y = Sub_appendix$Longitude)
