feeed6a0a2b890181e300e5cca86414a9dbc581f
gowachin <gowachin974@gmail.com>
2019-04-09 20:27:01 +0200

    vcf win

---
+++ CSV_to_VCF.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# tryhard :
+
+name=$1
+
+sed -i '1 i\ ##fileformat=VCFv4.1'   $name
+sed -i '2 i\ ##fileDate=20151126'   $name
+sed -i '3 i\ ##source=freeBayes v0.9.21-26-gbfd9832'   $name
+sed -i '4 i\ ##reference=./Pveris_genome/Pveris_pbjelly_updated_assembly_edited_all_ambig_turned_to_N.fasta'   $name
+sed -i '5 i\ ##phasing=none'   $name
+sed -i '6 i\ ##commandline="./freebayes/bin/freebayes --stdin -f ./Pveris_genome/Pveris_pbjelly_updated_assembly_edited_all_ambig_turned_to_N.fasta --min-coverage 20 -n 10 -m 30 -q 20 -F 0.3"'   $name
+sed -i '7 i\ ##filter="TYPE = snp"'   $name
+sed -i '8 i\ ##INFO=<ID=NS,Number=1,Type=Integer,Description="Number of samples with data">'   $name
+sed -i '9 i\ ##INFO=<ID=DP,Number=1,Type=Integer,Description="Total read depth at the locus">'   $name
+sed -i '10 i\ ##INFO=<ID=DPB,Number=1,Type=Float,Description="Total read depth per bp at the locus; bases in reads overlapping / bases in haplotype">'   $name
+sed -i '11 i\ ##INFO=<ID=AC,Number=A,Type=Integer,Description="Total number of alternate alleles in called genotypes">'   $name
+sed -i '12 i\ ##INFO=<ID=AN,Number=1,Type=Integer,Description="Total number of alleles in called genotypes">'   $name
+sed -i '13 i\ ##INFO=<ID=AF,Number=A,Type=Float,Description="Estimated allele frequency in the range (0,1]">'   $name
+sed -i '14 i\ ##INFO=<ID=RO,Number=1,Type=Integer,Description="Reference allele observation count, with partial observations recorded fractionally">'   $name
+sed -i '15 i\ ##INFO=<ID=AO,Number=A,Type=Integer,Description="Alternate allele observations, with partial observations recorded fractionally">'   $name
+sed -i '16 i\ ##INFO=<ID=PRO,Number=1,Type=Float,Description="Reference allele observation count, with partial observations recorded fractionally">'   $name
+sed -i '17 i\ ##INFO=<ID=PAO,Number=A,Type=Float,Description="Alternate allele observations, with partial observations recorded fractionally">'   $name
+sed -i '18 i\ ##INFO=<ID=QR,Number=1,Type=Integer,Description="Reference allele quality sum in phred">'   $name
+sed -i '19 i\ ##INFO=<ID=QA,Number=A,Type=Integer,Description="Alternate allele quality sum in phred">'   $name
+sed -i '20 i\ ##INFO=<ID=PQR,Number=1,Type=Float,Description="Reference allele quality sum in phred for partial observations">'   $name
+sed -i '21 i\ ##INFO=<ID=PQA,Number=A,Type=Float,Description="Alternate allele quality sum in phred for partial observations">'   $name
+sed -i '22 i\ ##INFO=<ID=SRF,Number=1,Type=Integer,Description="Number of reference observations on the forward strand">'   $name
+sed -i '23 i\ ##INFO=<ID=SRR,Number=1,Type=Integer,Description="Number of reference observations on the reverse strand">'   $name
+sed -i '24 i\ ##INFO=<ID=SAF,Number=A,Type=Integer,Description="Number of alternate observations on the forward strand">'   $name
+sed -i '25 i\ ##INFO=<ID=SAR,Number=A,Type=Integer,Description="Number of alternate observations on the reverse strand">'   $name
+sed -i '26 i\ ##INFO=<ID=SRP,Number=1,Type=Float,Description="Strand balance probability for the reference allele: Phred-scaled upper-bounds estimate of the probability of observing the deviation between SRF and SRR given E(SRF/SRR) ~ 0.5, derived using Hoeffdings inequality">'   $name
+sed -i '27 i\ ##INFO=<ID=SAP,Number=A,Type=Float,Description="Strand balance probability for the alternate allele: Phred-scaled upper-bounds estimate of the probability of observing the deviation between SAF and SAR given E(SAF/SAR) ~ 0.5, derived using Hoeffdings inequality">'   $name
+sed -i '28 i\ ##INFO=<ID=AB,Number=A,Type=Float,Description="Allele balance at heterozygous sites: a number between 0 and 1 representing the ratio of reads showing the reference allele to all reads, considering only reads from individuals called as heterozygous">'   $name
+sed -i '29 i\ ##INFO=<ID=ABP,Number=A,Type=Float,Description="Allele balance probability at heterozygous sites: Phred-scaled upper-bounds estimate of the probability of observing the deviation between ABR and ABA given E(ABR/ABA) ~ 0.5, derived using Hoeffdings inequality">'   $name
+sed -i '30 i\ ##INFO=<ID=RUN,Number=A,Type=Integer,Description="Run length: the number of consecutive repeats of the alternate allele in the reference genome">'   $name
+sed -i '31 i\ ##INFO=<ID=RPP,Number=A,Type=Float,Description="Read Placement Probability: Phred-scaled upper-bounds estimate of the probability of observing the deviation between RPL and RPR given E(RPL/RPR) ~ 0.5, derived using Hoeffdings inequality">'   $name
+sed -i '32 i\ ##INFO=<ID=RPPR,Number=1,Type=Float,Description="Read Placement Probability for reference observations: Phred-scaled upper-bounds estimate of the probability of observing the deviation between RPL and RPR given E(RPL/RPR) ~ 0.5, derived using Hoeffdings inequality">'   $name
+sed -i '33 i\ ##INFO=<ID=RPL,Number=A,Type=Float,Description="Reads Placed Left: number of reads supporting the alternate balanced to the left (5) of the alternate allele">'   $name
+sed -i '34 i\ ##INFO=<ID=RPR,Number=A,Type=Float,Description="Reads Placed Right: number of reads supporting the alternate balanced to the right (3) of the alternate allele">'   $name
+sed -i '35 i\ ##INFO=<ID=EPP,Number=A,Type=Float,Description="End Placement Probability: Phred-scaled upper-bounds estimate of the probability of observing the deviation between EL and ER given E(EL/ER) ~ 0.5, derived using Hoeffdings inequality">'   $name
+sed -i '36 i\ ##INFO=<ID=EPPR,Number=1,Type=Float,Description="End Placement Probability for reference observations: Phred-scaled upper-bounds estimate of the probability of observing the deviation between EL and ER given E(EL/ER) ~ 0.5, derived using Hoeffdings inequality">'   $name
+sed -i '37 i\ ##INFO=<ID=DPRA,Number=A,Type=Float,Description="Alternate allele depth ratio.  Ratio between depth in samples with each called alternate allele and those without.">'   $name
+sed -i '38 i\ ##INFO=<ID=ODDS,Number=1,Type=Float,Description="The log odds ratio of the best genotype combination to the second-best.">'   $name
+sed -i '39 i\ ##INFO=<ID=GTI,Number=1,Type=Integer,Description="Number of genotyping iterations required to reach convergence or bailout.">'   $name
+sed -i '40 i\ ##INFO=<ID=TYPE,Number=A,Type=String,Description="The type of allele, either snp, mnp, ins, del, or complex.">'   $name
+sed -i '41 i\ ##INFO=<ID=CIGAR,Number=A,Type=String,Description="The extended CIGAR representation of each alternate allele, with the exception that = is replaced by M to ease VCF parsing.  Note that INDEL alleles do not have the first matched base (which is provided by default, per the spec) referred to by the CIGAR.">'   $name
+sed -i '42 i\ ##INFO=<ID=NUMALT,Number=1,Type=Integer,Description="Number of unique non-reference alleles in called genotypes at this position.">'   $name
+sed -i '43 i\ ##INFO=<ID=MEANALT,Number=A,Type=Float,Description="Mean number of unique non-reference allele observations per sample with the corresponding alternate alleles.">'   $name
+sed -i '44 i\ ##INFO=<ID=LEN,Number=A,Type=Integer,Description="allele length">'   $name
+sed -i '45 i\ ##INFO=<ID=MQM,Number=A,Type=Float,Description="Mean mapping quality of observed alternate alleles">'   $name
+sed -i '46 i\ ##INFO=<ID=MQMR,Number=1,Type=Float,Description="Mean mapping quality of observed reference alleles">'   $name
+sed -i '47 i\ ##INFO=<ID=PAIRED,Number=A,Type=Float,Description="Proportion of observed alternate alleles which are supported by properly paired read fragments">'   $name
+sed -i '48 i\ ##INFO=<ID=PAIREDR,Number=1,Type=Float,Description="Proportion of observed reference alleles which are supported by properly paired read fragments">'   $name
+sed -i '49 i\ ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">'   $name
+sed -i '50 i\ ##FORMAT=<ID=GQ,Number=1,Type=Float,Description="Genotype Quality, the Phred-scaled marginal (or unconditional) probability of the called genotype">'   $name
+sed -i '51 i\ ##FORMAT=<ID=GL,Number=G,Type=Float,Description="Genotype Likelihood, log10-scaled likelihoods of the data given the called genotype for each possible genotype generated from the reference and alternate alleles given the sample ploidy">'   $name
+sed -i '52 i\ ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Read Depth">'   $name
+sed -i '53 i\ ##FORMAT=<ID=RO,Number=1,Type=Integer,Description="Reference allele observation count">'   $name
+sed -i '54 i\ ##FORMAT=<ID=QR,Number=1,Type=Integer,Description="Sum of quality of the reference observations">'   $name
+sed -i '55 i\ ##FORMAT=<ID=AO,Number=A,Type=Integer,Description="Alternate allele observation count">'   $name
+sed -i '56 i\ ##FORMAT=<ID=QA,Number=A,Type=Integer,Description="Sum of quality of the alternate observations">'   $name
+sed -i '57s/.*/#&/' $name
+
+#mv tryhard.csv tryhard.vcf ; ls
+
--- Erythrodrosum_vcf.R
+++ Erythrodrosum_vcf.R
@@ -1,30 +1,32 @@
 #install.packages("vcfR")
-library(vcfR)
+#library(vcfR)
 library(readr)
 
 #vcf.cov10.fullmat.SNP = read.vcfR("data_vcf/freebayes_-F0_3-n10-m13_-q20_mincov10_90samples_SNPs_only.vcf", checkFile = F)
 #vcf.cov20.fullmat.SNP = read.vcfR("data_vcf/freebayes_-F0_3-n10-m30_-q20_mincov20_90samples_SNPs_only.vcf", checkFile = F)
 
-mincov10_90samples_CSV <- read_delim("data_vcf/freebayes_-F0_3-n10-m13_-q20_mincov10_90samples_SNPs_only CSV.csv",
+mincov10_90samples_CSV <- read_delim("data_vcf/freebayes_-F0_3-n10-m13_-q20_mincov10_90samples_SNPs_onlyCSV.csv",
                                      "\t", escape_double = FALSE, trim_ws = TRUE)
 colnames(mincov10_90samples_CSV)
-mincov20_90samples_CSV <- read_delim("data_vcf/freebayes_-F0_3-n10-m30_-q20_mincov20_90samples_SNPs_only CSV.csv",
+mincov20_90samples_CSV <- read_delim("data_vcf/freebayes_-F0_3-n10-m30_-q20_mincov20_90samples_SNPs_onlyCSV.csv",
                                      "\t", escape_double = FALSE, trim_ws = TRUE)
 colnames(mincov20_90samples_CSV)
 
 colnames(mincov20_90samples_CSV) == colnames(mincov10_90samples_CSV) # mêmes colnames pour deux tableaux
-vcf = colnames(mincov10_90samples_CSV)[1:9]
+vcf = colnames(mincov20_90samples_CSV)[1:9]
 
-Eryth = c(vcf,'AP1','AMB','AML','AOL','CS1','CP1','CP4','DGB','DRL','DMB','HC1','HGL','HS2','HP1','HPB','PT1','PV1','GA2','GA4','VR3','VR1','VL2','VB1')
+Eryth = c(vcf,'AMB','AML','AOL','CS1','CP1','CP4','DGB','DRL','DMB','HC1','HGL','HS2','HP1','HPB','PT1','PV1','GA2','GA4','VR3','VR1','VL2','VB1')
+# without the outgroup: AP1 (P. lutea)
+Erythro_mincov10 = mincov10_90samples_CSV [,which(colnames(mincov10_90samples_CSV) %in% Eryth)]
 
-Erythro_mincov10 = mincov10_90samples_CSV [,which(colnames(mincov10_90samples_CSV) %in% Eryth)] # with one outgroup: AP1 (P. lutea)
+Erythro_mincov20 = mincov20_90samples_CSV [,which(colnames(mincov20_90samples_CSV) %in% Eryth)]
 
-Erythro_mincov20 = mincov20_90samples_CSV [,which(colnames(mincov20_90samples_CSV) %in% Eryth)] # with one outgroup: AP1 (P. lutea)
-
+rm(mincov10_90samples_CSV,mincov20_90samples_CSV )
 
 #permet de virer lignes vides ####
 clean = function(data) {
-  exterminate = apply(data[,10:32],1,paste,collapse ="")
+  n.col= dim(data)[2]
+  exterminate = apply(data[,10:n.col],1,paste,collapse ="")
   compte = exterminate == paste(rep(".",23),collapse = "")
   print(summary(compte))
   resum = c(pourc.clean = (1-sum(compte)/length(compte) )*100)
@@ -41,52 +43,139 @@ inform_mincov20 = clean(Erythro_mincov20) ; dim(inform_mincov20)
 #Mode   FALSE    TRUE
 #logical   84836     410
 
+rm(Erythro_mincov10,Erythro_mincov20)
+
 #permet de virer les lignes avec seulement un certain nombre de variants ####
-tri = function(data,n,quiet = F, r= F,p = F) { # data est un data frame type inform_mincov10 , n = pourcentage de présence du snp , quiet est le rendu
-  col = dim(data)[2]
-  matrix = data[,10:col]
+tri = function(data,n.r = 0,n.c = 0,quiet = F, r= F,p = F) { # data est un data frame type inform_mincov10 , n = pourcentage de présence du snp , quiet est le rendu
+  #tri sur les lignes
+  n.col = dim(data)[2] ; n.row = dim(data)[1]
+  matrix = data[,10:n.col]
   matrix = matrix == "."
+
   row  = apply(matrix,1,sum)
-  row = 1-row/(col-9) >= n
+  row = 1-row/(n.col-9) >= n.r
+
   data = data[which(row == T),]
 
-  resum = c(pourc.end = sum(row)/length(row)*100 ,
-            n.site = sum(row))
+  #tri sur les individus
+  n.col = dim(data)[2] ; n.row = dim(data)[1]
+  matrix = data[,10:n.col]
+  matrix = matrix == "."
+
+  col  = apply(matrix,2,sum)
+  col = 1-col/(n.row) >= n.c
+
+  matrix = data[,10:n.col]
+  matrix = matrix[,which(col == T)]
+  data = cbind(data[,1:9],matrix)
+
+  resum = c(r.pourc.end = sum(row)/length(row)*100 ,
+            r.n.site = sum(row),
+            c.pourc.end = sum(col)/length(col)*100 ,
+            c.n.ind = sum(col))
 
   if(quiet == F) print(resum)
   if (r== T & p == F) return(data)
   if (r== T & p == T) return(resum)
 }
 
+#tri(data = inform_mincov10,n.r=0.8, r = F)
 
-tri(data = inform_mincov10,n=0.8, r = F)
+test = tri(data = inform_mincov20,n.r=0.8,n.c = 0.8, r = T) # avec ces seuils on vire l'individu AML car trop d'info manquantes pour cet individu
 
-tri(data = inform_mincov20,n=0.8, r = F)
+plot(log(test$QUAL), cex = 0.1, ylim = c(log(0.001),20)) # virer des points avec pas assez bon Phred??? qu'est-ce qu'il représente??
 
-par(mfrow = c(2,1))
-perc = seq(0,1,by = 0.1)
-res = matrix(perc,nrow = length(perc), ncol = 3) ; colnames(res) = c("stringence","info.garde","n.sites")
+write.table(test,"data_vcf/tryhard.csv",sep = "\t", quote = F, row.names=F)
+system(" ./CSV_to_VCF.sh data_vcf/tryhard.csv ; mv data_vcf/tryhard.csv data_vcf/tryhard.vcf ; ls" )
+
+#./CSV_to_VCF.sh file.txt
+
+#mv tryhard.csv tryhard.vcf ; ls
+-----------
+
+# PGDSpider
+system("cd ; echo ########fichier home######## ; ls ;
+        cd Téléchargements/PGDSpider_2.1.1.5/ ; echo fichier PGDSpider2 ; ls ;
+       ./PGDSpider2.sh")
+
+
+library(LEA)
+library(ade4)
+library(vegan)
+
+source("http://membres-timc.imag.fr/Olivier.Francois/Conversion.R")
+source("http://membres-timc.imag.fr/Olivier.Francois/POPSutilities.R")
+
+struct2geno(file = "data_vcf/tryhard.str", TESS = FALSE, diploid = T, FORMAT = 2,extra.row = 1, extra.col = 2, output = "data_vcf/tryhard.geno")
+#ne marche pas car premiere ligne en trop
+#permet de créer le fichier format geno 23 individuals and 25086 markers. (SNP)
+
+obj  <- snmf("data/freebayes_23_str_rearranged.geno", K = 1:14, entropy = T, ploidy = 2,
+             CPU = 7,repetitions = 10, project= "new", alpha=100)
+# Choix du K optimal (20 runs)
+ID =rearanged$ind.names
+
+par(mfrow = c(1,1))
+plot(obj, col = "blue", pch=1,cex=0.8)
+beep(3)
+color = c("orange","violet","lightgreen","red","blue","green","cyan","grey","black","yellow","darkgreen")
+
+obj.snmf = snmf("data/freebayes_23_str_rearranged.geno", K = 11, alpha = 100, project = "new")
+qmatrix = Q(obj.snmf, K = 11)
+barplot(t(qmatrix), col = color, border = NA, space = 0, xlab = "Individuals", ylab = "Admixture coefficients",
+        names.arg =ID, las = 2)
+
+Pop = function(K) {
+obj.snmf = snmf("data/freebayes_23_str_rearranged.geno", K = K, alpha = 100, project = "new",
+                CPU = 7)
+qmatrix = Q(obj.snmf, K = K)
+barplot(t(qmatrix), col = color, border = NA, space = 0,xlab = "Individuals", ylab = "Admixture coefficients",
+        names.arg =ID, las = 2)}
+
+par(mfrow = c(3,4))
+for (i in 1:12) Pop(i) ;beep(3)
+# adegenet ####
+library(parallel)
+
+
+# plot de l'évolution du pourcentage de données en fonction des seuils ####
+par(mfrow = c(2,2))
+#seuil de lignes
+perc = seq(0,1,by = 0.05)
+res = matrix(perc,nrow = length(perc), ncol = 5) ; colnames(res) = c("stringence","info.garde","n.sites","info.garde","n.ind")
 for( i in 1:length(perc)) {
-  res[i,2:3] = tri(data = inform_mincov10,n=perc[i], r = T,p=T)
+  res[i,2:5] = tri(data = inform_mincov10,n.r=perc[i], r = T,p=T)
 }
-plot(x = res[,1], y = res[,2], xlab = "seuil de stringence", ylab = "info gardée")
+plot(x = res[,1], y = res[,2], xlab = "seuil de stringence", ylab = "info gardée", main = "seuil par lignes")
 abline(h = 5, col = "red") ; text(x = res[,1], y = res[,2], res[,3], pos = 3)
 
-perc = seq(0,1,by = 0.1)
-res = matrix(perc,nrow = length(perc), ncol = 3) ; colnames(res) = c("stringence","info.garde","n.sites")
+perc = seq(0,1,by = 0.05)
+res = matrix(perc,nrow = length(perc), ncol = 5) ; colnames(res) = c("stringence","info.garde","n.sites","info.garde","n.ind")
 for( i in 1:length(perc)) {
-  res[i,2:3] = tri(data = inform_mincov20,n=perc[i], r = T,p=T)
+  res[i,2:5] = tri(data = inform_mincov20,n.r=perc[i], r = T,p=T)
 }
-plot(x = res[,1], y = res[,2], xlab = "seuil de stringence", ylab = "info gardée")
+plot(x = res[,1], y = res[,2], xlab = "seuil de stringence", ylab = "info gardée", main = "seuil par lignes")
 abline(h = 5, col = "red") ; text(x = res[,1], y = res[,2], res[,3], pos = 3)
+
+#seuil d'ind
+perc = seq(0,1,by = 0.05)
+res = matrix(perc,nrow = length(perc), ncol = 5) ; colnames(res) = c("stringence","info.garde","n.sites","info.garde","n.ind")
+for( i in 1:length(perc)) {
+  res[i,2:5] = tri(data = inform_mincov10,n.c=perc[i], r = T,p=T)
+}
+plot(x = res[,1], y = res[,4], xlab = "seuil de stringence", ylab = "info gardée", main = "seuil par ind")
+abline(h = 5, col = "red") ; text(x = res[,1], y = res[,4], res[,5], pos = 3)
+
+perc = seq(0,1,by = 0.05)
+res = matrix(perc,nrow = length(perc), ncol = 5) ; colnames(res) = c("stringence","info.garde","n.sites","info.garde","n.ind")
+for( i in 1:length(perc)) {
+  res[i,2:5] = tri(data = inform_mincov20,n.c=perc[i], r = T,p=T)
+}
+plot(x = res[,1], y = res[,4], xlab = "seuil de stringence", ylab = "info gardée", main = "seuil par ind")
+abline(h = 5, col = "red") ; text(x = res[,1], y = res[,4], res[,5], pos = 3)
 par(mfrow = c(1,1))
 
-system("cd data_vcf ; ls ;
-       sed -i '1 i\##fileformat=VCFv4.0' 'freebayes_-F0_3-n10-m13_-q20_mincov10_23samples_SNPs_only' " )
 
-tryhard = tri(data = inform_mincov20,n=1, r = T)
-plot(log(tryhard$QUAL)) ; abline(h = log(0.3), col = "red")
-dim(tryhard[which(tryhard$QUAL > 20),])
-dim(tryhard)
 
-write.csv(tryhard, file = "tryhardtest.csv")
+
+
